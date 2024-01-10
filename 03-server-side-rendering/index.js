d2cf6b983dfcf62a91be885e0d6f3d82d937a9a8
Derek <derek.baker89@gmail.com>
2024-01-10 00:10:46 +0000

    return html when hitting '/' route

--- 03-server-side-rendering/index.js
+++ 03-server-side-rendering/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 8000;
 
-app.get("/", (res) => {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
