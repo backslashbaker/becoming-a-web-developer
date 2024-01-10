81e959fa093a5e3aa777161cd1ef397814802904
Derek <derek.baker89@gmail.com>
2024-01-10 00:29:42 +0000

    create static file folder

--- 03-server-side-rendering/index.js
+++ 03-server-side-rendering/index.js
@@ -5,7 +5,11 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "assests/index.html"));
+});
+
+app.get("/css/style.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "assests/mystyle.css"));
 });
 
 app.listen(port, () => {
